026a0b8af3e9081bb509f85d6d651204b60f9f4b
Aldo Sequera <aldo@example.com>
2025-05-08 23:28:46 +0700

    Initial commit

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,41 +1,58 @@
-'use client'
-
-import {
-  UserGroupIcon,
-  HomeIcon,
-  DocumentDuplicateIcon,
-} from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
+import Image from 'next/image';
+import { lusitana } from '@/app/ui/fonts';
+import { LatestInvoice } from '@/app/lib/definitions';
 
-const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
-];
-
-export default function NavLinks() {
-  const pathname = usePathname();
+export default async function LatestInvoices({
+  latestInvoices,
+}: {
+  latestInvoices: LatestInvoice[];
+}) {
   return (
-    <>
-      {links.map((link) => {
-        const LinkIcon = link.icon;
-        return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={clsx('flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3')}
-          >
-            <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
-          </Link>
-        );
-      })}
-    </>
+    <div className="flex w-full flex-col md:col-span-4">
+      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Latest Invoices
+      </h2>
+
+      <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
+        <div className="bg-white px-6">
+          {latestInvoices.map((invoice, i) => (
+            <div
+              key={invoice.id}
+              className={clsx('flex flex-row items-center justify-between py-4', {
+                'border-t': i !== 0,
+              })}
+            >
+              <div className="flex items-center">
+                <Image
+                  src={invoice.image_url}
+                  alt={`${invoice.name}'s profile picture`}
+                  className="mr-4 rounded-full"
+                  width={32}
+                  height={32}
+                />
+                <div className="min-w-0">
+                  <p className="truncate text-sm font-semibold md:text-base">
+                    {invoice.name}
+                  </p>
+                  <p className="hidden text-sm text-gray-500 sm:block">
+                    {invoice.email}
+                  </p>
+                </div>
+              </div>
+              <p className={`${lusitana.className} truncate text-sm font-medium md:text-base`}>
+                {invoice.amount}
+              </p>
+            </div>
+          ))}
+        </div>
+
+        <div className="flex items-center pb-2 pt-6">
+          <ArrowPathIcon className="h-5 w-5 text-gray-500" />
+          <h3 className="ml-2 text-sm text-gray-500">Updated just now</h3>
+        </div>
+      </div>
+    </div>
   );
 }
